87f973c57c96375b400727905feff75e435f6bee
Zhang Yao <cineazhan@gmail.com>
2023-12-10 20:31:38 +0800

    dev: 美化

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Search } from "@element-plus/icons-vue"
+</script>
 
 <template>
   <div class="app-container">
-    <el-menu class="app-menu text-xl font-normal gap-3" mode="horizontal" :ellipsis="false">
+    <div class="h-[40px] flex items-end gap-5 font-normal text-lg">
       <div class="grow" />
       <div>首页</div>
       <div>景点</div>
       <div>攻略</div>
       <div class="grow" />
-    </el-menu>
+    </div>
+    <div class="px-3 mt-3">
+      <el-input size="large" placeholder="Please Input" :suffix-icon="Search" />
+    </div>
     <div class="p-5 app-footer">
       <el-button type="primary" size="large" class="w-full">测试按钮</el-button>
     </div>
@@ -19,24 +24,11 @@
 body {
   background-size: cover;
   background: #ddf3ff;
-  background: url("/mesh.png") no-repeat fixed;
+  background: url("/mesh.png") no-repeat center center fixed;
 
   font-weight: lighter;
 }
 .app-container {
   min-height: 200vh;
-}
-.app-menu {
-  height: 60px;
-  background: rgba(255, 255, 255, 0.3);
-  backdrop-filter: blur(5px);
-
-  align-items: end;
-  padding-bottom: 6px;
-  padding-left: 12px;
-  padding-right: 12px;
-
-  top: 0;
-  position: sticky;
 }
 </style>
